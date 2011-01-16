fe8573ca63a99cfb329741a0e0f7b81318b60ac0
Jon Moses <jon@burningbush.us>
2011-01-15 20:34:07 -0500

    Syncing really works now.

--- sync.rb
+++ sync.rb
@@ -5,38 +5,48 @@ require 'fileutils'
 
 app = Appscript.app('iTunes')
 
-playlists = ['Android'] #, 'All Recently Added']
+playlists = ['Android', 'All Recently Added'] #, 'All Recently Added']
 
 itunes_home = "/Users/jmoses/Music/iTunes/iTunes Media/Music/"
 
-dest = "/tmp/testing/"
+dest = ARGV[0]
+dest += '/' unless dest[-1] == '/'
 
-files_in_dest = Dir["#{dest}/**/*"].map {|f| next if File.directory?(f); f.gsub(%r|#{dest}|, '').gsub(/^\/+/, '') }.compact
+files_in_dest = Dir["#{dest}/**/*"].map {|f| next if File.directory?(f) || File.extname(f) == '.m3u'; f.gsub(%r|#{dest}|, '').gsub(/^\/+/, '') }.compact
 
 files_in_playlists = []
 
+copy_count = 0
+
 playlists.each do |playlist|
-  puts playlist
-  app.playlists[playlist].tracks.get.each do |track|
-    source = track.location.get.to_s
+  playlist_fname = "#{playlist}.m3u"
+  File.open( File.join(dest, playlist_fname), 'w') do |out|
+    app.playlists[playlist].tracks.get.each do |track|
+      source = track.location.get.to_s
 
-    filename = File.basename(source)
-    relative_source = source.gsub( %r|#{itunes_home}|, '')
+      filename = File.basename(source)
+      relative_source = source.gsub( %r|#{itunes_home}|, '')
 
-    files_in_playlists << relative_source
+      out.puts relative_source
 
-    target = File.join(dest, relative_source)
+      files_in_playlists << relative_source
 
-    unless File.exists?( target )
-      puts "Copying #{relative_source}"
+      target = File.join(dest, relative_source)
 
-      FileUtils.mkdir_p(File.dirname(target)) unless File.exists?(File.dirname(target))
-      FileUtils.cp( source, File.join(dest, relative_source) )
+      unless File.exists?( target )
+        copy_count += 1
+        FileUtils.mkdir_p(File.dirname(target)) unless File.exists?(File.dirname(target))
+        FileUtils.cp( source, File.join(dest, relative_source) )
+      end
     end
   end
 end
 
 files_to_delete = files_in_dest - files_in_playlists
 
-puts "Found #{files_to_delete.size} to delete"
+files_to_delete.each do |f|
+  FileUtils.rm( File.join( dest, f) )
+end
+
+puts "#{copy_count} files copied, #{files_to_delete.size} files pruned"
 
