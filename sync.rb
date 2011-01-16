083f13a74e6ae2ed15e546d50355097b30e13557
Jon Moses <jon@burningbush.us>
2011-01-15 21:01:19 -0500

    output things.  otherwise it's a question of is it doing anything.

--- sync.rb
+++ sync.rb
@@ -19,6 +19,8 @@ files_in_playlists = []
 copy_count = 0
 
 playlists.each do |playlist|
+  puts playlist
+
   playlist_fname = "#{playlist}.m3u"
   File.open( File.join(dest, playlist_fname), 'w') do |out|
     app.playlists[playlist].tracks.get.each do |track|
@@ -34,6 +36,7 @@ playlists.each do |playlist|
       target = File.join(dest, relative_source)
 
       unless File.exists?( target )
+        puts target
         copy_count += 1
         FileUtils.mkdir_p(File.dirname(target)) unless File.exists?(File.dirname(target))
         FileUtils.cp( source, File.join(dest, relative_source) )
@@ -44,7 +47,9 @@ end
 
 files_to_delete = files_in_dest - files_in_playlists
 
+puts "Pruning stale files..."
 files_to_delete.each do |f|
+  puts f
   FileUtils.rm( File.join( dest, f) )
 end
 
